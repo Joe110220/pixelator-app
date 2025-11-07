5de2862ed9d8fb450a0e2e5e892e40bcf823c305
Joe110220 <99229284+Joe110220@users.noreply.github.com>
2025-11-08 04:28:06 +0800

    Add files via upload

---
+++ electron/preload.ts
@@ -0,0 +1,32 @@
+import { contextBridge, ipcRenderer } from "electron";
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  saveImage: (filename: string, buffer: Buffer) =>
+    ipcRenderer.invoke("save-image", filename, buffer),
+  loadImage: (filename: string) =>
+    ipcRenderer.invoke("load-image", filename),
+  listImages: () =>
+    ipcRenderer.invoke("list-images"),
+  deleteImage: (filename: string) =>
+    ipcRenderer.invoke("delete-image", filename),
+  openFileDialog: () =>
+    ipcRenderer.invoke("open-file-dialog"),
+  saveFileDialog: (defaultName: string) =>
+    ipcRenderer.invoke("save-file-dialog", defaultName),
+  writeFile: (filePath: string, buffer: Buffer) =>
+    ipcRenderer.invoke("write-file", filePath, buffer)
+});
+
+declare global {
+  interface Window {
+    electronAPI: {
+      saveImage: (filename: string, buffer: Buffer) => Promise<void>;
+      loadImage: (filename: string) => Promise<Uint8Array | null>;
+      listImages: () => Promise<string[]>;
+      deleteImage: (filename: string) => Promise<void>;
+      openFileDialog: () => Promise<string | null>;
+      saveFileDialog: (defaultName?: string) => Promise<string | null>;
+      writeFile: (filePath: string, buffer: Buffer) => Promise<void>;
+    };
+  }
+}
